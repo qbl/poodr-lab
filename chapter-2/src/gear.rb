81858144d0c1edd41ccf9036421de675e2c133ed
qbl <farabi.iqbal@gmail.com>
2019-02-20 19:42:36 +1100

    Added gear_inches method

--- chapter-2/src/gear.rb
+++ chapter-2/src/gear.rb
@@ -1,16 +1,25 @@
 class Gear
-  attr_reader :chainrig, :cog
+  attr_reader :chainrig, :cog, :rim, :tire
 
-  def initialize(chainrig, cog)
+  def initialize(chainrig, cog, rim, tire)
     @chainrig = chainrig
     @cog = cog
+    @rim = rim
+    @tire = tire
   end
 
   def ratio
     chainrig / cog.to_f
   end
+
+  def gear_inches
+    ratio * (rim + (tire * 2))
+  end
 end
 
-puts Gear.new(52, 11).ratio
-puts Gear.new(30, 27).ratio
+puts Gear.new(52, 11, 26, 1.5).ratio
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+
+puts Gear.new(30, 27, 24, 1.25).ratio
+puts Gear.new(30, 27, 24, 1.25).gear_inches
 
